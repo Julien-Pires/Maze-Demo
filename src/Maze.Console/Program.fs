c31fa2fbc9f4d09ff26275801e22a3174c71653a
Julien Pires <piresjulien95@gmail.com>
2019-08-07 21:55:39 +0200

    Add demos solution

---
+++ src/Maze.Console/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open FSharp.Control
+open Maze
+
+[<EntryPoint>]
+let main _ =
+    async {
+        let game = Game.start()
+        let rec loop () = async {
+            let! result = game.Receive()
+            match result with
+            | Action action ->
+                match action with
+                | Input -> game.Post <| Entry(Console.ReadLine())
+            | Message msg -> printfn "%s" msg
+            
+            return! loop()
+        }
+        
+        return! loop()
+    } |> Async.RunSynchronously   
+    
+    0 // return an integer exit code
